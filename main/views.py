71c822985fa1a75b05df951616df7563fc0af020
Scarletra <fernandovalentino10@gmail.com>
2023-09-12 19:04:12 +0700

    finishing creating all files

--- main/views.py
+++ main/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 def show_main(request):
     context = {
         'Name': 'World Lock',
-        'Amount': '5'
+        'Amount': '5',
+        'Description': 'A currency that used usually on a trading'
     }
 
     return render(request, "main.html", context)
